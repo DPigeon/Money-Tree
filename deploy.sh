5ee935413b2f850be5841ab1414559c2fd938b4c
DPigeon <D_Pig@encs.concordia.ca>
2020-10-10 17:58:36 -0400

    Seperating into 2 containers

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd home/node &&
-envsubst '\$PORT' < ./etc/nginx/conf.d/default.conf.template > ./etc/nginx/conf.d/default.conf && 
-nginx -g 'daemon off;' &&
-cd ../maven &&
-java -jar ./jar/moneytree.jar
