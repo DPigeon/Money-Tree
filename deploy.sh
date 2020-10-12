fbe7baf9f843923f8a7560fe86d2bdb6d56f1019
DPigeon <D_Pig@encs.concordia.ca>
2020-10-12 17:26:38 -0400

    Final Deployment done

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker rm -f frontend &>/dev/null && echo 'Removed old frontend container.'
-docker rm -f backend &>/dev/null && echo 'Removed old frontend container.'
-docker login docker.pkg.github.com -u $1 -p $2
-docker pull docker.pkg.github.com/dpigeon/money-tree/frontend:latest
-docker pull docker.pkg.github.com/dpigeon/money-tree/backend:latest
-docker run -dit -p 80:80 --name frontend docker.pkg.github.com/dpigeon/money-tree/frontend:latest
-docker run -dit -p 8080:8080 --name backend docker.pkg.github.com/dpigeon/money-tree/backend:latest
