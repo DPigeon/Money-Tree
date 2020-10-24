c1c8aec022d4ae7b8fcd97a5fa712dc6873f54b8
abdulrahim <amansour@coveo.com>
2020-10-24 12:51:10 -0400

    implemented StockMarketDataFacade methods for batch, book, quote, company, news, keystats, chart and logo - linked with StockController and StockMarketDataService #109

--- backend/src/main/java/com/capstone/moneytree/controller/StockController.java
+++ backend/src/main/java/com/capstone/moneytree/controller/StockController.java
@@ -8,39 +8,76 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.capstone.moneytree.service.api.StockService;
-import pl.zankowski.iextrading4j.api.stocks.Quote;
+import pl.zankowski.iextrading4j.api.stocks.*;
 import pl.zankowski.iextrading4j.api.stocks.v1.BatchStocks;
+import pl.zankowski.iextrading4j.api.stocks.v1.KeyStats;
+import pl.zankowski.iextrading4j.api.stocks.v1.News;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/stockmarket")
 public class StockController extends ApiController {
 
-   private StockService stockService;
    @Autowired
    private StockMarketDataService stockMarketDataService;
    private static final Logger LOG = LoggerFactory.getLogger(StockController.class);
 
-   @Autowired
-   public StockController(StockService stockService){
-      this.stockService = stockService;
+   /**
+    * Obtain json object containing company quote, info, news, keystats and logo
+    *
+    * @param symbol
+    * @return batchStocks
+    */
+   @GetMapping("/batch/{symbol}")
+   public ResponseEntity<BatchStocks> getBatchStocksBySymbol(@Valid @PathVariable(name = "symbol") String symbol) {
+      BatchStocks batchStocks = stockMarketDataService.getBatchStocksBySymbol(symbol);
+      return ResponseEntity.ok(batchStocks);
+   }
+
+   @GetMapping("/book/{symbol}")
+   public ResponseEntity<Book> getBook(@PathVariable(name = "symbol") @Valid @NotBlank @Size(max = 5) String symbol) {
+      Book book = stockMarketDataService.getBook(symbol);
+      return ResponseEntity.ok(book);
    }
 
    @GetMapping("/quote/{symbol}")
    public ResponseEntity<Quote> getQuote(@PathVariable(name = "symbol") @Valid @NotBlank @Size(max = 5) String symbol) {
-      Quote quote = stockMarketDataService.getQuoteBySymbol(symbol);
+      Quote quote = stockMarketDataService.getQuote(symbol);
       return ResponseEntity.ok(quote);
    }
 
-   //not available for free tier...
-   @GetMapping("/batch/{symbols}")
-   public ResponseEntity<BatchStocks> getBatchBySymbols(@Valid @PathVariable(name = "symbols") String symbols) {
-      BatchStocks batchStocks = stockMarketDataService.getBatchStocksBySymbols(symbols);
-      return ResponseEntity.ok(batchStocks);
+   @GetMapping("/company/{symbol}")
+   public ResponseEntity<Company> getCompanyInfo(@PathVariable(name = "symbol") @Valid @NotBlank @Size(max = 5) String symbol) {
+      Company companyInfo = stockMarketDataService.getCompanyInfo(symbol);
+      return ResponseEntity.ok(companyInfo);
    }
 
+   @GetMapping("/news/{symbol}/{n}")
+   public ResponseEntity<List<News>> getLastNNews(@PathVariable(name = "symbol") @Valid @NotBlank @Size(max = 5) String symbol,
+                                               @PathVariable(name = "n") @Valid int n ) {
+      List<News> news = stockMarketDataService.getLastNNews(symbol, n);
+      return ResponseEntity.ok(news);
+   }
 
+   @GetMapping("/keystats/{symbol}")
+   public ResponseEntity<KeyStats> getKeyStats(@PathVariable(name = "symbol") @Valid @NotBlank @Size(max = 5) String symbol) {
+      KeyStats keystats = stockMarketDataService.getKeyStats(symbol);
+      return ResponseEntity.ok(keystats);
+   }
+
+   @GetMapping("/chart/{symbol}/{range}")
+   public ResponseEntity<List<Chart>> getChart(@PathVariable(name = "symbol") @Valid @NotBlank @Size(max = 5) String symbol, @PathVariable(name = "range") @Valid String range) {
+      List<Chart> chart = stockMarketDataService.getChart(symbol, range.toUpperCase());
+      return ResponseEntity.ok(chart);
+   }
+
+   @GetMapping("/logo/{symbol}")
+   public ResponseEntity<Logo> getLogo(@PathVariable(name = "symbol") @Valid @NotBlank @Size(max = 5) String symbol) {
+      Logo logo = stockMarketDataService.getLogo(symbol);
+      return ResponseEntity.ok(logo);
+   }
 }
--- backend/src/main/java/com/capstone/moneytree/service/api/StockMarketDataService.java
+++ backend/src/main/java/com/capstone/moneytree/service/api/StockMarketDataService.java
@@ -1,13 +1,20 @@
 package com.capstone.moneytree.service.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import pl.zankowski.iextrading4j.api.stocks.Quote;
+import pl.zankowski.iextrading4j.api.stocks.*;
 import pl.zankowski.iextrading4j.api.stocks.v1.BatchStocks;
+import pl.zankowski.iextrading4j.api.stocks.v1.KeyStats;
+import pl.zankowski.iextrading4j.api.stocks.v1.News;
 
 import java.util.List;
 
 public interface StockMarketDataService {
 
-    Quote getQuoteBySymbol(String symbol);
-    BatchStocks getBatchStocksBySymbols(String symbols);
+    BatchStocks getBatchStocksBySymbol(String symbols);
+    Book getBook(String symbol);
+    Quote getQuote(String symbol);
+    Company getCompanyInfo(String symbol);
+    List<News> getLastNNews(String symbol, int lastN);
+    KeyStats getKeyStats(String symbol);
+    List<Chart> getChart(String symbol, String range);
+    Logo getLogo(String symbol);
 }
