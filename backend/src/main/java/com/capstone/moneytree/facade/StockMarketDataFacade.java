e7ccf5ae732b4570830b91aa657126c72a3c5dab
abdulrahim <amansour@coveo.com>
2020-10-24 12:51:10 -0400

    implemented StockMarketDataFacade methods for batch, book, quote, company, news, keystats, chart and logo - linked with StockController and StockMarketDataService #109

--- backend/src/main/java/com/capstone/moneytree/facade/StockMarketDataFacade.java
+++ backend/src/main/java/com/capstone/moneytree/facade/StockMarketDataFacade.java
@@ -1,27 +1,25 @@
 package com.capstone.moneytree.facade;
 
-
-import com.capstone.moneytree.controller.StockController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import pl.zankowski.iextrading4j.api.stocks.Quote;
+import pl.zankowski.iextrading4j.api.stocks.*;
+import pl.zankowski.iextrading4j.api.stocks.v1.KeyStats;
 import pl.zankowski.iextrading4j.api.stocks.v1.BatchStocks;
+import pl.zankowski.iextrading4j.api.stocks.v1.News;
 import pl.zankowski.iextrading4j.client.IEXCloudClient;
 import pl.zankowski.iextrading4j.client.IEXCloudTokenBuilder;
 import pl.zankowski.iextrading4j.client.IEXTradingApiVersion;
 import pl.zankowski.iextrading4j.client.IEXTradingClient;
-import pl.zankowski.iextrading4j.client.rest.manager.RestRequest;
-import pl.zankowski.iextrading4j.client.rest.request.IEXCloudV1RestRequest;
-import pl.zankowski.iextrading4j.client.rest.request.stocks.QuoteRequestBuilder;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.*;
 import pl.zankowski.iextrading4j.client.rest.request.stocks.v1.BatchStocksRequestBuilder;
 import pl.zankowski.iextrading4j.client.rest.request.stocks.v1.BatchStocksType;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.v1.KeyStatsRequestBuilder;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.v1.NewsRequestBuilder;
 
 import java.util.List;
-
 
 /**
  * This facade abstracts the IEXCloud API and exposes only the relevant
@@ -33,8 +31,6 @@ public class StockMarketDataFacade {
    private final IEXCloudClient stockMarketDataClient;
    private static final Logger LOG = LoggerFactory.getLogger(StockMarketDataFacade.class);
 
-   // controller -> service -> facade -> api
-
    @Autowired
    public StockMarketDataFacade(@Value("${IEXCloud.publishable.token}") String PUBLISH_TOKEN, @Value("${IEXCloud.secret.token}") String SECRET_TOKEN) {
       stockMarketDataClient = IEXTradingClient.create(IEXTradingApiVersion.IEX_CLOUD_V1,
@@ -42,6 +38,31 @@ public class StockMarketDataFacade {
                       .withPublishableToken(PUBLISH_TOKEN)
                       .withSecretToken(SECRET_TOKEN)
                 .build());
+   }
+
+   public BatchStocks getBatchStocksBySymbol(String symbol) {
+      final BatchStocks batchStocks = stockMarketDataClient.executeRequest(new BatchStocksRequestBuilder()
+              .withSymbol(symbol)
+              .addType(BatchStocksType.BOOK)
+              .addType(BatchStocksType.COMPANY)
+              .addType(BatchStocksType.NEWS).withLast(10)
+              .addType(BatchStocksType.KEY_STATS)
+              .addType(BatchStocksType.LOGO)
+              .addType(BatchStocksType.CHART)
+              .build()
+      );
+
+      LOG.info("Fetch batch: {}", batchStocks.toString());
+      return batchStocks;
+   }
+
+   public Book getBook(String symbol) {
+      final Book book = stockMarketDataClient.executeRequest(new BookRequestBuilder()
+              .withSymbol(symbol)
+              .build());
+
+      LOG.info("Fetched book: {}", book.toString());
+      return book;
    }
 
    public Quote getQuote(String symbol) {
@@ -53,16 +74,63 @@ public class StockMarketDataFacade {
       return quote;
    }
 
-   public BatchStocks getBatchStocksBySymbols(String symbols) {
-      final BatchStocks batchStocks = stockMarketDataClient.executeRequest(new BatchStocksRequestBuilder()
-              .withSymbol(symbols)
-              .addType(BatchStocksType.COMPANY)
-              .addType(BatchStocksType.QUOTE)
-              .addType(BatchStocksType.PRICE)
-              .addType(BatchStocksType.NEWS)
-              .build()
-      );
-      LOG.info("Fetch batch: {}", batchStocks.getCompany().getSymbol());
-      return batchStocks;
+   public Company getCompanyInfo(String symbol) {
+      final Company company = stockMarketDataClient.executeRequest(new CompanyRequestBuilder()
+         .withSymbol(symbol)
+         .build());
+
+      LOG.info("Fetched company: {}", company.toString());
+      return company;
+   }
+
+   public List<News> getLastNNews(String symbol, int lastN) {
+      final List<News> news = stockMarketDataClient.executeRequest(new NewsRequestBuilder()
+              .withSymbol(symbol)
+              .withLast(lastN)
+              .build());
+
+      for (News n : news) {
+         LOG.info("Fetched news: {}", n.toString());
+      }
+      return news;
+   }
+
+   public KeyStats getKeyStats(String symbol) {
+      final KeyStats keyStats = stockMarketDataClient.executeRequest(new KeyStatsRequestBuilder()
+         .withSymbol(symbol)
+         .build());
+
+      LOG.info("Fetched key stats: {}", keyStats.toString());
+      return keyStats;
+   }
+
+   public List<Chart> getChart(String symbol, String range) {
+
+      ChartRange chartRange = ChartRange.YEAR_TO_DATE;
+      if (range.equals("DAY"))
+         chartRange = ChartRange.ONE_DAY;
+      else if (range.equals("WEEK"))
+         chartRange = ChartRange.FIVE_DAYS;
+      else if (range.equals("MONTH"))
+         chartRange = ChartRange.ONE_MONTH;
+      else if (range.equals("YEAR"))
+         chartRange = ChartRange.ONE_YEAR;
+
+      final List<Chart> charts = stockMarketDataClient.executeRequest(new ChartRequestBuilder()
+              .withSymbol(symbol)
+              .withChartRange(chartRange)
+              .build());
+
+      LOG.info("Fetched charts: {}", charts.toString());
+      return charts;
+   }
+
+   public Logo getLogo(String symbol) {
+      final Logo logo = stockMarketDataClient.executeRequest(new LogoRequestBuilder()
+              .withSymbol(symbol)
+              .build());
+
+      LOG.info("Fetched logo: {}", logo.toString());
+      return logo;
    }
 }
--- backend/src/main/java/com/capstone/moneytree/service/impl/DefaultStockMarketDataService.java
+++ backend/src/main/java/com/capstone/moneytree/service/impl/DefaultStockMarketDataService.java
@@ -5,8 +5,10 @@ import com.capstone.moneytree.service.api.StockMarketDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import pl.zankowski.iextrading4j.api.stocks.Quote;
+import pl.zankowski.iextrading4j.api.stocks.*;
 import pl.zankowski.iextrading4j.api.stocks.v1.BatchStocks;
+import pl.zankowski.iextrading4j.api.stocks.v1.KeyStats;
+import pl.zankowski.iextrading4j.api.stocks.v1.News;
 
 import java.util.List;
 
@@ -18,12 +20,42 @@ public class DefaultStockMarketDataService implements StockMarketDataService {
     StockMarketDataFacade stockMarketDataFacade;
 
     @Override
-    public Quote getQuoteBySymbol(String symbol) {
+    public BatchStocks getBatchStocksBySymbol(String symbol) {
+        return stockMarketDataFacade.getBatchStocksBySymbol(symbol);
+    }
+
+    @Override
+    public Book getBook(String symbol) {
+        return stockMarketDataFacade.getBook(symbol);
+    }
+
+    @Override
+    public Quote getQuote(String symbol) {
         return stockMarketDataFacade.getQuote(symbol);
     }
 
     @Override
-    public BatchStocks getBatchStocksBySymbols(String symbols) {
-        return stockMarketDataFacade.getBatchStocksBySymbols(symbols);
+    public Company getCompanyInfo(String symbol) {
+        return stockMarketDataFacade.getCompanyInfo(symbol);
+    }
+
+    @Override
+    public List<News> getLastNNews(String symbol, int lastN) {
+        return stockMarketDataFacade.getLastNNews(symbol, lastN);
+    }
+
+    @Override
+    public KeyStats getKeyStats(String symbol) {
+        return stockMarketDataFacade.getKeyStats(symbol);
+    }
+
+    @Override
+    public List<Chart> getChart(String symbol, String range) {
+        return stockMarketDataFacade.getChart(symbol, range);
+    }
+
+    @Override
+    public Logo getLogo(String symbol) {
+        return stockMarketDataFacade.getLogo(symbol);
     }
 }
