a1ad9f9202a00b4356b3daae727655cbd854fafd
DPigeon <D_Pig@encs.concordia.ca>
2020-10-25 14:21:51 -0400

    #108: Add account, positions and portfolio facade

--- backend/src/main/java/com/capstone/moneytree/facade/MarketInteractionsFacade.java
+++ backend/src/main/java/com/capstone/moneytree/facade/MarketInteractionsFacade.java
@@ -2,10 +2,19 @@ package com.capstone.moneytree.facade;
 
 import net.jacobpeterson.alpaca.AlpacaAPI;
 
+import net.jacobpeterson.alpaca.enums.PortfolioPeriodUnit;
+import net.jacobpeterson.alpaca.enums.PortfolioTimeFrame;
+import net.jacobpeterson.alpaca.rest.exception.AlpacaAPIRequestException;
+import net.jacobpeterson.domain.alpaca.account.Account;
+import net.jacobpeterson.domain.alpaca.portfoliohistory.PortfolioHistory;
+import net.jacobpeterson.domain.alpaca.position.Position;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 /**
  * This facade abstracts the Alpaca API and exposes only the relevant
@@ -21,5 +30,65 @@ public class MarketInteractionsFacade {
    @Autowired
    public MarketInteractionsFacade() {
       alpacaAPI = new AlpacaAPI();
+   }
+
+   /**
+    * Gets the Alpaca user account.
+    * @return An Account.
+    */
+   public Account getAccount() {
+      Account account = null;
+      try {
+         account = alpacaAPI.getAccount();
+         LOGGER.info("Get account: {}", toString().replace(",", ",\n\t"));
+      } catch (AlpacaAPIRequestException e) {
+         e.printStackTrace();
+      }
+
+      return account;
+   }
+
+   /**
+    * Gets all stocks position for a user.
+    * @return List of available positions.
+    */
+   public ArrayList<Position> getOpenPositions() {
+      ArrayList<Position> positions = null;
+      try {
+         positions = alpacaAPI.getOpenPositions();
+      } catch (AlpacaAPIRequestException e) {
+         e.printStackTrace();
+      }
+
+      return positions;
+   }
+
+   /**
+    * Gets the timeseries data for the profile value of equity and profit loss.
+    * @param periodLength Duration of the data.
+    * @param periodUnit Either day (D), week (W), month (M) or year (A).
+    * @param timeFrame Resolution of the time window (1Min, 5Min, 15Min, 1H, 1D)
+    * @param dateEnd Date data is returned up to.
+    * @param extendedHours Includes extended hours in result. Works only for timeframe less than 1D.
+    * @return
+    */
+   public PortfolioHistory getPortfolioHistory(int periodLength, String periodUnit,
+                                               String timeFrame, LocalDate dateEnd,
+                                               boolean extendedHours) {
+      PortfolioHistory portfolioHistory = null;
+      PortfolioPeriodUnit portfolioPeriodUnit = PortfolioPeriodUnit.valueOf(periodUnit);
+      PortfolioTimeFrame portfolioTimeFrame = PortfolioTimeFrame.valueOf(timeFrame);
+      try {
+         portfolioHistory = alpacaAPI.getPortfolioHistory(
+                 periodLength,
+                 portfolioPeriodUnit,
+                 portfolioTimeFrame,
+                 dateEnd,
+                 extendedHours);
+      } catch (AlpacaAPIRequestException e) {
+         e.printStackTrace();
+      }
+
+      return portfolioHistory;
    }
 }
