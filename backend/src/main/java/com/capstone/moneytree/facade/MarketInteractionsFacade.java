369f17affdbeeee95d28824a2cbb5aa5cbc3d931
DPigeon <D_Pig@encs.concordia.ca>
2020-10-24 22:44:36 -0400

    #108: Add Order descriptions

--- backend/src/main/java/com/capstone/moneytree/facade/MarketInteractionsFacade.java
+++ backend/src/main/java/com/capstone/moneytree/facade/MarketInteractionsFacade.java
@@ -2,18 +2,24 @@ package com.capstone.moneytree.facade;
 
 import net.jacobpeterson.alpaca.AlpacaAPI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * This facade abstracts the Alpaca api and exposes only the relevant
+ * This facade abstracts the Alpaca API and exposes only the relevant
  * methods for MoneyTree.
  * */
+
 @Component
 public class MarketInteractionsFacade {
 
-   // private final AlpacaAPI alpacaAPI = new AlpacaAPI(); To use in the future.
+   private static final Logger LOGGER = LoggerFactory.getLogger(MarketInteractionsFacade.class);
+   private final AlpacaAPI alpacaAPI;
 
-
-   public MarketInteractionsFacade(){/* Empty for now*/}
-
+   @Autowired
+   public MarketInteractionsFacade() {
+      alpacaAPI = new AlpacaAPI();
+   }
 }
--- backend/src/main/java/com/capstone/moneytree/model/decorator/StopOrderDecorator.java
+++ backend/src/main/java/com/capstone/moneytree/model/decorator/StopOrderDecorator.java
@@ -5,11 +5,19 @@ import lombok.Builder;
 import lombok.NoArgsConstructor;
 import net.jacobpeterson.domain.alpaca.order.Order;
 
+/**
+ * The StopOrder Decorator for AlpacaAPI.
+ * A stop (market) order is an order to buy or sell a security when its price moves past a
+ * particular point, ensuring a higher probability of achieving a predetermined entry or exit price.
+ */
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class StopOrderDecorator extends OrderDecorator {
+
    public Order order;
+
    @Override
    public void decorate() {
       //Do something with order to make it a StopOrderDecorator
