81e97114c0d7a7f52100eeba3122814e62530c49
arthur-del <arthur.tourneyrie@gmail.com>
2020-10-30 11:15:37 -0400

    created stockDetail page and route #34

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StockDetailComponent } from './pages/stock-detail/stock-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'stock-detail/:ticker', component: StockDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
