377b5b000cfbd3a83e1c08f4ec4419d55fedfd05
EspressoCode <hn.84@yahoo.com>
2020-11-02 12:53:38 -0800

    refactor stockHeader to presentation component #34

--- client/src/app/components/stock-detail-header/stock-detail-header.component.ts
+++ client/src/app/components/stock-detail-header/stock-detail-header.component.ts
@@ -1,29 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Stock } from './../../interfaces/stock';
 
 @Component({
   selector: 'app-stock-detail-header',
   templateUrl: './stock-detail-header.component.html',
   styleUrls: ['./stock-detail-header.component.scss']
 })
-export class StockDetailHeaderComponent implements OnInit {
-  public companyName = ""
-  public stockChange: number;
-  public stockChangePercent: number;
-  public stockValue: number;
+export class StockDetailHeaderComponent implements OnInit{
+  @Input() stockInfo: Stock;
   constructor() { }
 
-  ngOnInit(): void {
-    this.companyName = 'Air canada';
-    this.stockChange = 4.27;
-    this.stockChangePercent = 1.68;
-    this.stockValue = 16.36;
-  }
+  ngOnInit(): void {}
 
   stockChangeColor() {
-    return this.stockChange < 0 ? 'negative-change' : 'positive-change';
+    return this.stockInfo.stockChange < 0 ? 'negative-change' : 'positive-change';
   }
   stockInfoFormatter() {
-    let sign = this.stockChange < 0 ? '-' : '+';
-    return sign + this.stockChange + "("+ this.stockChangePercent+"%)";
+    let sign = this.stockInfo.stockChange < 0 ? '-' : '+';
+    return sign + this.stockInfo.stockChange + "("+ this.stockInfo.stockChangePercent+"%)";
   }
 }
