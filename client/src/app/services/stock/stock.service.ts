8f964554f4b63d02fed5b32807e47b3df6d41cab
arthur-del <arthur.tourneyrie@gmail.com>
2020-11-02 17:29:52 -0500

    set up ngrx store for stock data #34

--- client/src/app/services/stock/stock.service.ts
+++ client/src/app/services/stock/stock.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Stock } from 'src/app/interfaces/stock';
 import { ApiService } from '../api/api.service';
 
 @Injectable({
@@ -6,5 +8,21 @@ import { ApiService } from '../api/api.service';
 })
 export class StockService {
 
+  // To be deleted
+  stockInfo: Stock = {
+    tickerSymbol: 'AC',
+    companyName: 'Air Canada',
+    industry: 'Transportation',
+    volatility: 'High',
+    stockChange: 4.27,
+    stockChangePercent: 1.68,
+    stockValue: 16.36,
+  }
+
   constructor(private api: ApiService) { }
+
+  loadStockInfo(stockTicker: string):Observable<Stock>{
+    //http call
+    return of(this.stockInfo);
+  }
 }
--- client/src/app/store/selectors/app.selectors.ts
+++ client/src/app/store/selectors/app.selectors.ts
@@ -1,2 +1,5 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { State } from '../reducers/app.reducer';
+import { Stock } from 'src/app/interfaces/stock';
 
+export const selectLoadedStock = (state: State) => state.currentStockLoaded;
