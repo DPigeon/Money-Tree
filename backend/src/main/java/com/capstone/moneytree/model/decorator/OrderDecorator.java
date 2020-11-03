b28c3634c932592885c0aadf88c004733208bb2f
razine-bensari <bensaria97@gmail.com>
2020-11-02 20:25:13 -0500

    Issue #99: Addressing PR comments

--- backend/src/main/java/com/capstone/moneytree/model/decorator/OrderDecorator.java
+++ backend/src/main/java/com/capstone/moneytree/model/decorator/OrderDecorator.java
@@ -1,12 +1,8 @@
 package com.capstone.moneytree.model.decorator;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
 import net.jacobpeterson.domain.alpaca.order.Order;
 
-@Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public abstract class OrderDecorator extends Order {
 
