a89be9b3a6b509e2f3ac374f4a157c9e5e898c62
DPigeon <D_Pig@encs.concordia.ca>
2020-11-12 22:46:57 -0500

    #135: Tests for Creating Account

--- backend/src/main/java/com/capstone/moneytree/service/impl/DefaultUserService.java
+++ backend/src/main/java/com/capstone/moneytree/service/impl/DefaultUserService.java
@@ -3,7 +3,6 @@ package com.capstone.moneytree.service.impl;
 import com.capstone.moneytree.dao.UserDao;
 import com.capstone.moneytree.model.node.User;
 import com.capstone.moneytree.service.api.UserService;
-import com.capstone.moneytree.utils.MoneyTreePasswordEncryption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +16,11 @@ import java.util.List;
 public class DefaultUserService implements UserService {
 
     private final UserDao userDao;
-    private final MoneyTreePasswordEncryption passwordEncryption;
     private static final Logger LOG = LoggerFactory.getLogger(DefaultUserService.class);
 
     @Autowired
     public DefaultUserService(UserDao userDao) {
         this.userDao = userDao;
-        this.passwordEncryption = new MoneyTreePasswordEncryption();
     }
 
     @Override
@@ -33,37 +30,11 @@ public class DefaultUserService implements UserService {
 
     @Override
     public User getUserById(Long id) {
-        LOG.info("Retrieved user ID {}", id);
         return userDao.findUserById(id);
-    }
-
-    /**
-     * Find a unique User with email and username
-     * @param email The email of the user
-     * @param username The username of the user
-     * @return The User in the database
-     */
-    @Override
-    public User getUserByEmailAndUsername(String email, String username) {
-        List<User> userList = userDao.findAll();
-        User userToFind = null;
-
-        for (User user : userList) {
-            if (email.equalsIgnoreCase(user.getEmail()) || username.equalsIgnoreCase(user.getUsername())) {
-                userToFind = user;
-                LOG.info("Found user with email {}", user.getEmail());
-                break;
-            }
-        }
-
-        return userToFind;
     }
 
     @Override
     public User createUser(User user) {
-        String password = user.getPassword();
-        String encryptedPassword = encryptData(password);
-        user.setPassword(encryptedPassword);
         userDao.save(user);
         LOG.info("Created user: {}", user.getFirstName());
 
@@ -79,45 +50,7 @@ public class DefaultUserService implements UserService {
     @Override
     public boolean userExists(String email, String username) {
         boolean exists = false;
-        User user = userDao.findUserByEmailAndUsername(email, username);
-
-        if (user != null) {
-            exists = true;
-            LOG.info("User already exists.");
-        }
-        return exists;
-    }
-
-    @Override
-    public User registerAlpacaApiKey(Long id, String key) {
-        User userToUpdate = userDao.findUserById(id);
-        if (userToUpdate != null) {
-            userToUpdate.setAlpacaApiKey(key);
-            userDao.save(userToUpdate);
-            LOG.info("Registered Alpaca key for user email {}", userToUpdate.getEmail());
-        }
-
-        return userToUpdate;
-    }
-
-    public String encryptData(String text) {
-        return passwordEncryption.toGraphProperty(text);
-    }
-
-    public boolean compareDigests(String plainPassword, String encryptedPassword) {
-        return passwordEncryption.checkPassword(plainPassword, encryptedPassword);
-    }
-
-    /**
-     * Method to look if user exists with unique email and username.
-     * @param email The email coming from the frontend.
-     * @param username The username coming from the frontend.
-     * @return boolean if exists or not.
-     */
-    @Override
-    public boolean userExists(String email, String username) {
-        boolean exists = false;
-        Iterable<User> userList = userDao.findAll();
+        List<User> userList = userDao.findAll();
 
         for (User user : userList) {
             String dataEmail = user.getEmail();
