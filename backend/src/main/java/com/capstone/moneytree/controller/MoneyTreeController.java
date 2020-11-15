c7264a017d93488f8b4abb845cad4f74b121f302
razine-bensari <bensaria97@gmail.com>
2020-11-14 21:26:33 -0500

    Fixing root api path

--- backend/src/main/java/com/capstone/moneytree/controller/MoneyTreeController.java
+++ backend/src/main/java/com/capstone/moneytree/controller/MoneyTreeController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/api/v1")
 public @interface MoneyTreeController {
 }
