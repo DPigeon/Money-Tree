25faf37f396b8bf4f12b1579278c85b579ed7acc
marwan011 <marwan.ayadi@aheeva.com>
2020-11-15 13:20:22 -0500

    #142 add stats object to Stock

--- client/src/app/interfaces/stock.ts
+++ client/src/app/interfaces/stock.ts
@@ -1,10 +1,20 @@
 export interface Stock {
-    tickerSymbol: string;
-    companyName: string;
-    industry: string;
-    volatility: string;
-    stockChange: number;
-    stockChangePercent: number;
-    stockValue: number;
-    logo: string;
+  tickerSymbol: string;
+  companyName: string;
+  industry: string;
+  volatility: string;
+  stockChange: number;
+  stockChangePercent: number;
+  stockValue: number;
+  logo: string;
+  stats: {
+    open: number;
+    high: number;
+    low: number;
+    volume: number;
+    mktCap: number;
+    wh: number;
+    wl: number;
+    avgVolume: number;
+  };
 }
