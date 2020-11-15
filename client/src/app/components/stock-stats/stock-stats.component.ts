bf818f8f9186eb919dd160f144674567beb569a2
marwan011 <marwan.ayadi@aheeva.com>
2020-11-15 13:56:32 -0500

    #142 set stats to 2 decimals

--- client/src/app/components/stock-stats/stock-stats.component.ts
+++ client/src/app/components/stock-stats/stock-stats.component.ts
@@ -11,34 +11,34 @@ export class StockStatsComponent {
   constructor() {}
 
   get open(): string | number {
-    return this.stockInfo ? this.stockInfo.stats.open : '';
+    return this.stockInfo ? this.stockInfo.stats.open.toFixed(2) : '';
   }
 
   get high(): string | number {
-    return this.stockInfo ? this.stockInfo.stats.high : '';
+    return this.stockInfo ? this.stockInfo.stats.high.toFixed(2) : '';
   }
 
   get low(): string | number {
-    return this.stockInfo ? this.stockInfo.stats.low : '';
+    return this.stockInfo ? this.stockInfo.stats.low.toFixed(2) : '';
   }
 
   get volume(): string | number {
-    return this.stockInfo ? this.stockInfo.stats.volume : '';
+    return this.stockInfo ? this.stockInfo.stats.volume.toPrecision(3) : '';
   }
 
   get mktCap(): string | number {
-    return this.stockInfo ? this.stockInfo.stats.mktCap : '';
+    return this.stockInfo ? this.stockInfo.stats.mktCap.toFixed(2) : '';
   }
 
   get wh(): string | number {
-    return this.stockInfo ? this.stockInfo.stats.wh : '';
+    return this.stockInfo ? this.stockInfo.stats.wh.toFixed(2) : '';
   }
 
   get wl(): string | number {
-    return this.stockInfo ? this.stockInfo.stats.wl : '';
+    return this.stockInfo ? this.stockInfo.stats.wl.toFixed(2) : '';
   }
 
   get avgVolume(): string | number {
-    return this.stockInfo ? this.stockInfo.stats.avgVolume : '';
+    return this.stockInfo ? this.stockInfo.stats.avgVolume.toPrecision(3) : '';
   }
 }
