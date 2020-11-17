f489663f651c3887318c0c73c60e4ac76b6e4eb3
EspressoCode <hn.84@yahoo.com>
2020-11-17 14:36:06 -0800

    fix header styling #100

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -4,14 +4,14 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  encapsulation: ViewEncapsulation.None, // disable ViewEncapsulation to be able to show menu in mobile version
+  // encapsulation: ViewEncapsulation.None, // disable ViewEncapsulation to be able to show menu in mobile version
 })
 export class HeaderComponent implements OnInit {
   isInMobileMode: boolean;
   constructor() {}
 
   ngOnInit(): void {
-    const isMobile = (window.innerWidth <= 600 ) && ( window.innerHeight <= 600 );
+    const isMobile = (window.innerWidth <= 600 );
     this.isInMobileMode = isMobile;
   }
 
