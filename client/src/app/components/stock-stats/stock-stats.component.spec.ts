d902372ce8a4574db2367e76cbdc12db9a563f72
marwan011 <marwan.ayadi@aheeva.com>
2020-11-18 12:13:15 -0500

    #142 validate stats props + update test

--- client/src/app/components/stock-stats/stock-stats.component.spec.ts
+++ client/src/app/components/stock-stats/stock-stats.component.spec.ts
@@ -35,7 +35,16 @@ const stockInfo: Stock = {
   stockChangePercent: 1.68,
   stockValue: 16.36,
   logo: 'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0021/6847/brand.gif?itok=u0iVoArk',
-  stats: null
+  stats: {
+    open: 100.50,
+    high: 200.20,
+    low: 516510,
+    volume: 151515,
+    mktCap: 51651,
+    wh: 455,
+    wl: 123,
+    avgVolume: 199410,
+  },
 };
 
 // unit tests
@@ -49,6 +58,10 @@ describe('StockStats', () => {
   it('should display correct stock open stat', () => {
     component.stockInfo = stockInfo;
     expect(component.open).toBe(component.stockInfo.stats.open.toFixed(2));
+    component.stockInfo.stats.open = stockInfo.stats.open;
+    expect(component.open).toBe(component.stockInfo.stats.open.toFixed(2));
+    component.stockInfo.stats.open = null;
+    expect(component.open).toBe('');
     component.stockInfo = null;
     expect(component.open).toBe('');
   });
@@ -56,6 +69,10 @@ describe('StockStats', () => {
   it('should display correct stock high stat', () => {
     component.stockInfo = stockInfo;
     expect(component.high).toBe(component.stockInfo.stats.high.toFixed(2));
+    component.stockInfo.stats.high = stockInfo.stats.high;
+    expect(component.high).toBe(component.stockInfo.stats.high.toFixed(2));
+    component.stockInfo.stats.high = null;
+    expect(component.high).toBe('');
     component.stockInfo = null;
     expect(component.high).toBe('');
   });
@@ -63,13 +80,21 @@ describe('StockStats', () => {
   it('should display correct stock low stat', () => {
     component.stockInfo = stockInfo;
     expect(component.low).toBe(component.stockInfo.stats.low.toFixed(2));
+    component.stockInfo.stats.low = stockInfo.stats.low;
+    expect(component.low).toBe(component.stockInfo.stats.low.toFixed(2));
+    component.stockInfo.stats.low = null;
+    expect(component.low).toBe('');
     component.stockInfo = null;
     expect(component.low).toBe('');
   });
 
   it('should display correct stock volume stat', () => {
     component.stockInfo = stockInfo;
-    expect(component.volume).toBe(component.stockInfo.stats.volume.toPrecision(2));
+    expect(component.volume).toBe(component.stockInfo.stats.volume.toPrecision(3));
+    component.stockInfo.stats.volume = stockInfo.stats.volume;
+    expect(component.volume).toBe(component.stockInfo.stats.volume.toPrecision(3));
+    component.stockInfo.stats.volume = null;
+    expect(component.volume).toBe('');
     component.stockInfo = null;
     expect(component.volume).toBe('');
   });
@@ -77,6 +102,10 @@ describe('StockStats', () => {
   it('should display correct stock mktCap stat', () => {
     component.stockInfo = stockInfo;
     expect(component.mktCap).toBe(component.stockInfo.stats.mktCap.toFixed(2));
+    component.stockInfo.stats.mktCap = stockInfo.stats.mktCap;
+    expect(component.mktCap).toBe(component.stockInfo.stats.mktCap.toFixed(2));
+    component.stockInfo.stats.mktCap = null;
+    expect(component.mktCap).toBe('');
     component.stockInfo = null;
     expect(component.mktCap).toBe('');
   });
@@ -84,6 +113,10 @@ describe('StockStats', () => {
   it('should display correct stock wl stat', () => {
     component.stockInfo = stockInfo;
     expect(component.wl ).toBe(component.stockInfo.stats.wl.toFixed(2));
+    component.stockInfo.stats.wl = stockInfo.stats.wl;
+    expect(component.wl ).toBe(component.stockInfo.stats.wl.toFixed(2));
+    component.stockInfo.stats.wl = null;
+    expect(component.wl).toBe('');
     component.stockInfo = null;
     expect(component.wl).toBe('');
   });
@@ -91,13 +124,21 @@ describe('StockStats', () => {
   it('should display correct stock wh stat', () => {
     component.stockInfo = stockInfo;
     expect(component.wh).toBe(component.stockInfo.stats.wh.toFixed(2));
+    component.stockInfo.stats.wh = stockInfo.stats.wh;
+    expect(component.wh).toBe(component.stockInfo.stats.wh.toFixed(2));
+    component.stockInfo.stats.wh= null;
+    expect(component.wh).toBe('');
     component.stockInfo = null;
     expect(component.wh).toBe('');
   });
 
   it('should display correct stock avg volume stat', () => {
     component.stockInfo = stockInfo;
-    expect(component.avgVolume).toBe(component.stockInfo.stats.avgVolume.toPrecision(2));
+    expect(component.avgVolume).toBe(component.stockInfo.stats.avgVolume.toPrecision(3));
+    component.stockInfo.stats.avgVolume = stockInfo.stats.avgVolume;
+    expect(component.avgVolume).toBe(component.stockInfo.stats.avgVolume.toPrecision(3));
+    component.stockInfo.stats.avgVolume = null;
+    expect(component.avgVolume).toBe('');
     component.stockInfo = null;
     expect(component.avgVolume).toBe('');
   });
--- client/src/app/components/stock-stats/stock-stats.component.ts
+++ client/src/app/components/stock-stats/stock-stats.component.ts
@@ -11,34 +11,34 @@ export class StockStatsComponent {
   constructor() {}
 
   get open(): string  {
-    return this.stockInfo ? this.stockInfo.stats.open.toFixed(2) : '';
+    return this.stockInfo ? this.stockInfo.stats.open ? this.stockInfo.stats.open.toFixed(2) : '' : '';
   }
 
   get high(): string  {
-    return this.stockInfo ? this.stockInfo.stats.high.toFixed(2) : '';
+    return this.stockInfo ? this.stockInfo.stats.high ? this.stockInfo.stats.high.toFixed(2) : '' : '';
   }
 
   get low(): string  {
-    return this.stockInfo ? this.stockInfo.stats.low.toFixed(2) : '';
+    return this.stockInfo ? this.stockInfo.stats.low ? this.stockInfo.stats.low.toFixed(2) : '' : '';
   }
 
   get volume(): string  {
-    return this.stockInfo ? this.stockInfo.stats.volume.toPrecision(3) : '';
+    return this.stockInfo ?  this.stockInfo.stats.volume ? this.stockInfo.stats.volume.toPrecision(3) : '' : '';
   }
 
   get mktCap(): string  {
-    return this.stockInfo ? this.stockInfo.stats.mktCap.toFixed(2) : '';
+    return this.stockInfo ? this.stockInfo.stats.mktCap ? this.stockInfo.stats.mktCap.toFixed(2) : '' : '';
   }
 
   get wh(): string  {
-    return this.stockInfo ? this.stockInfo.stats.wh.toFixed(2) : '';
+    return this.stockInfo ? this.stockInfo.stats.wh ? this.stockInfo.stats.wh.toFixed(2) : '' : '';
   }
 
   get wl(): string  {
-    return this.stockInfo ? this.stockInfo.stats.wl.toFixed(2) : '';
+    return this.stockInfo ? this.stockInfo.stats.wl ? this.stockInfo.stats.wl.toFixed(2) : '' : '';
   }
 
   get avgVolume(): string  {
-    return this.stockInfo ? this.stockInfo.stats.avgVolume.toPrecision(3) : '';
+    return this.stockInfo ? this.stockInfo.stats.avgVolume ? this.stockInfo.stats.avgVolume.toPrecision(3) : '' : '';
   }
 }
