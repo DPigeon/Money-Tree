d5509bd979bccf1f0d3aba486f1da890cdb35d9b
abdulrahim <amansour@coveo.com>
2020-11-23 23:03:33 -0500

    refactored error handling for uploading image to s3 when editing user profile and added logic to delete previous image if exists #145

--- backend/src/main/java/com/capstone/moneytree/exception/FailedUploadImageS3Exception.java
+++
@@ -1,7 +0,0 @@
-package com.capstone.moneytree.exception;
-
-public class FailedUploadImageS3Exception extends RuntimeException {
-    public FailedUploadImageS3Exception(String message) {
-        super(message);
-    }
-}
