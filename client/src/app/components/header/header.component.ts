d2a3c7e1d23c9384067cbcdfc02fc0c57912a244
Alessandro <kalessandro14@gmail.com>
2020-11-26 15:23:09 -0500

    added logout functionality #138

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreFacadeService } from '../../store/store-facade.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor(private storeFacade: StoreFacadeService) {}
 
   ngOnInit(): void {}
+
+  logout() {
+    this.storeFacade.logCurrentUserOut();
+  }
 }
