c18bda45ae4653bbe0ba897029d71ea4c62a59af
Alessandro <kalessandro14@gmail.com>
2020-12-01 15:01:13 -0500

    added unit tests to the store facade #138

--- client/src/app/store/store-facade.service.spec.ts
+++ client/src/app/store/store-facade.service.spec.ts
@@ -4,6 +4,23 @@ import { StoreFacadeService } from './store-facade.service';
 import * as appActions from './actions/app.actions';
 import { NGRX_STORE_MODULE } from '../shared.module';
 
+const userInfo = {
+  id: 1,
+  firstName: "John",
+  lastName: "Doe",
+  username: "john1",
+  avatarUrl: "",
+  email: "john1@gmail.com",
+  score: 12,
+  rank: 10000,
+  balance: 223,
+  alpacaApiKey: null,
+  follows: [],
+  followers: [],
+  portfolio: [],
+  transactions: []
+}
+
 describe('StoreFacadeService', () => {
   let service: StoreFacadeService;
   let store: MockStore;
@@ -31,4 +48,34 @@ describe('StoreFacadeService', () => {
       appActions.loadStockInfo({ stockTicker: 'TESLA' })
     );
   });
+
+  it('should dispatach create new user', () => {
+    const spy = jest.spyOn(store, 'dispatch');
+    service.createNewUser(userInfo)
+    expect(spy).toHaveBeenCalledWith(appActions.createNewUser({ user: userInfo }));
+  })
+
+  it('should dispatach user login', () => {
+    const spy = jest.spyOn(store, 'dispatch');
+    service.userLogin(userInfo)
+    expect(spy).toHaveBeenCalledWith(appActions.userLogin({ user: userInfo }));
+  })
+
+  it('should dispatach update user', () => {
+    const spy = jest.spyOn(store, 'dispatch');
+    service.updateUser(userInfo)
+    expect(spy).toHaveBeenCalledWith(appActions.upadateUser({ user: userInfo }));
+  })
+
+  it('should dispatach get current user', () => {
+    const spy = jest.spyOn(store, 'dispatch');
+    service.getCurrentUser(userInfo.id)
+    expect(spy).toHaveBeenCalledWith(appActions.getCurrentUser({ id: userInfo.id }));
+  })
+
+  it('should dispatach log user out', () => {
+    const spy = jest.spyOn(store, 'dispatch');
+    service.logCurrentUserOut();
+    expect(spy).toHaveBeenCalledWith(appActions.logCurrentUserOut());
+  })
 });
