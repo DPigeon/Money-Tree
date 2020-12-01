55dea9674322a977315e0ecd1456d130854bf8cc
Alessandro <kalessandro14@gmail.com>
2020-12-01 12:14:46 -0500

    refactored login-signup into methods #138

--- client/src/app/pages/login-signup/login-signup.component.ts
+++ client/src/app/pages/login-signup/login-signup.component.ts
@@ -6,45 +6,67 @@ import { StoreFacadeService } from '../../store/store-facade.service';
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
-  styleUrls: ['./login-signup.component.scss']
+  styleUrls: ['./login-signup.component.scss'],
 })
 export class LoginSignupComponent implements OnInit {
   hasAlpacaCode: boolean = false;
   hasAppError = this.storeFacade.appError$;
 
-  constructor(private storeFacade: StoreFacadeService, private route: ActivatedRoute, private router: Router) {
+  constructor(
+    private storeFacade: StoreFacadeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-    this.storeFacade.shouldAlpacaRedirect$.subscribe(shouldRedirect =>{
-      if(shouldRedirect && !this.hasAlpacaCode) {
-        this.registerWithAlpaca();
-      }
+  ngOnInit() {
+    this.storeFacade.shouldAlpacaRedirect$.subscribe((shouldRedirect: boolean) => {
+      this.handleAlpacaRedirectResponse(shouldRedirect);
     });
 
-    this.storeFacade.isUserLoggedIn$.subscribe(isUserLoggedIn =>{
-      if(isUserLoggedIn) {
-        this.router.navigate(['/home']);
-      }
-    })
+    this.storeFacade.isUserLoggedIn$.subscribe((isUserLoggedIn: boolean) => {
+      this.handleLoginRedirect(isUserLoggedIn);
+    });
 
-    this.route.queryParams.subscribe(params => {
-      const alpacaCode = params['code'];
-      this.hasAlpacaCode = false;
-      if(!!alpacaCode) {
-        this.hasAlpacaCode = true;
-        const userParams: User = {id: Number(localStorage.getItem('userId')), alpacaApiKey: alpacaCode};
-        this.storeFacade.updateUser(userParams);
-      }
+    this.route.queryParams.subscribe((routeData) => {
+      this.handleRouteData(routeData);
     });
   }
 
-  ngOnInit(): void {
+  handleAlpacaRedirectResponse(shouldRedirect: boolean) {
+    if (shouldRedirect && !this.hasAlpacaCode) {
+      this.registerWithAlpaca();
+    }
+  }
+
+  handleLoginRedirect(isUserLoggedIn: boolean) {
+    if (isUserLoggedIn) {
+      this.router.navigate(['/home']);
+    }
+  }
+
+  handleRouteData(routeData) {
+    const alpacaCode = routeData ? routeData['code'] : null;
+    this.hasAlpacaCode = false;
+    if (!!alpacaCode) {
+      this.hasAlpacaCode = true;
+      const userParams: User = {
+        id: Number(localStorage.getItem('userId')),
+        alpacaApiKey: alpacaCode,
+      };
+      this.storeFacade.updateUser(userParams);
+    }
   }
 
   registerWithAlpaca() {
     // TODO: This values need to be hidden / updated
-    const redirectUri = 'http://localhost:4200/'//EnvironmentVariables.ALPACA_REDIRECT_URL;
-    const clientId = '198903d0d2f523a25e4dce65837bbf0d';//EnvironmentVariables.ALPACA_CLIENT_ID;
-    const alpacaUrl = 'https://app.alpaca.markets/oauth/authorize?response_type=code&client_id='+clientId+'&redirect_uri='+redirectUri+'&scope=trading'
+    const redirectUri = 'http://localhost:4200/'; //EnvironmentVariables.ALPACA_REDIRECT_URL;
+    const clientId = '198903d0d2f523a25e4dce65837bbf0d'; //EnvironmentVariables.ALPACA_CLIENT_ID;
+    const alpacaUrl =
+      'https://app.alpaca.markets/oauth/authorize?response_type=code&client_id=' +
+      clientId +
+      '&redirect_uri=' +
+      redirectUri +
+      '&scope=trading';
     window.location.href = alpacaUrl;
   }
 
