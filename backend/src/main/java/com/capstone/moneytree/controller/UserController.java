7dad23078ed33f1f5058ab84b8061cacd08477fb
DPigeon <D_Pig@encs.concordia.ca>
2020-12-12 14:32:42 -0500

    #183: Fix code smells for backend

--- backend/src/main/java/com/capstone/moneytree/controller/UserController.java
+++ backend/src/main/java/com/capstone/moneytree/controller/UserController.java
@@ -10,9 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 
 import javax.security.auth.login.CredentialNotFoundException;
 
