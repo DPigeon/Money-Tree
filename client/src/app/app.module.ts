83acefc277f5779dafa92c6e4807d9c0d9bea21b
arthur-del <arthur.tourneyrie@gmail.com>
2020-12-12 10:51:03 -0500

    style fixes merge fixes #181

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,15 +17,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { MatDialogModule } from '@angular/material/dialog';
-=======
-import { MatDialogModule } from '@angular/material/dialog'
->>>>>>> 1ee7bdd489aa1e97423da4a0a74fd88b00481293
-
-
-
-
 
 
 
