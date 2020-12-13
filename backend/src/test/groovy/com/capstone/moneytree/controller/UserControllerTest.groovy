d34c1eed379c876ce8f564f8c46de5b61c6a3700
wltrf <walter.fleury@gmail.com>
2020-12-13 16:14:45 -0500

    Update tests to use real database #165
    Add @autowired which will be instantiated using application-*.properties. for dev it will use local db. for github it will use uat db
    Remove mocking
    Add some cleanup to existing tests

--- backend/src/test/groovy/com/capstone/moneytree/controller/UserControllerTest.groovy
+++ backend/src/test/groovy/com/capstone/moneytree/controller/UserControllerTest.groovy
@@ -1,51 +1,41 @@
 package com.capstone.moneytree.controller
 
-import static com.capstone.moneytree.utils.MoneyTreeTestUtils.createUsersInMockedDatabase
-import static com.capstone.moneytree.utils.MoneyTreeTestUtils.createUser
-import static com.capstone.moneytree.utils.MoneyTreeTestUtils.createCredential
-
-import org.springframework.mock.web.MockHttpServletRequest
-import org.springframework.web.context.request.RequestContextHolder
-import org.springframework.web.context.request.ServletRequestAttributes
-
-import com.capstone.moneytree.dao.UserDao
+import com.capstone.moneytree.MoneytreeApplication
 import com.capstone.moneytree.exception.EntityNotFoundException
 import com.capstone.moneytree.exception.MissingMandatoryFieldException
 import com.capstone.moneytree.handler.exception.UserAlreadyExistsException
 import com.capstone.moneytree.model.node.User
-import com.capstone.moneytree.service.api.UserService
-import com.capstone.moneytree.service.impl.DefaultUserService
-import com.capstone.moneytree.validator.UserValidator
-import com.capstone.moneytree.validator.ValidatorFactory
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
+import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
 import spock.lang.Specification
 
 import javax.security.auth.login.CredentialNotFoundException
 
+import static com.capstone.moneytree.utils.MoneyTreeTestUtils.createCredential
+import static com.capstone.moneytree.utils.MoneyTreeTestUtils.createUser
 /**
  * Unit Tests for the User Controller.
  */
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MoneytreeApplication.class)
+@EnableConfigurationProperties
 class UserControllerTest extends Specification {
 
    private static final String URL_LOCATION = "http://localhost/users/"
 
-   private static UserDao userDao
-   private static UserValidator userValidator
-   private static ValidatorFactory validatorFactory
-   private static UserService defaultUserService
-   private static UserController userController
+   @Autowired
+   private final UserController userController
 
    def setup() {
-      userDao = Mock()
-      userValidator = new UserValidator(userDao)
-      validatorFactory = Mock(ValidatorFactory) {
-         it.getUserValidator() >> userValidator
-      }
-      defaultUserService = new DefaultUserService(userDao, validatorFactory)
-      userController = new UserController(defaultUserService)
       MockHttpServletRequest request = new MockHttpServletRequest()
       RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request))
    }
@@ -60,19 +50,15 @@ class UserControllerTest extends Specification {
       String lastName = "Doe"
       User user = createUser(email, username, password, firstName, lastName, null)
 
-      and: "mock the database with some users already registered"
-      userDao.findAll() >> createUsersInMockedDatabase()
-      userDao.findUserByEmailAndUsername(user.getEmail(), user.getUsername()) >> null
-
-      and: "mock the database to save the user"
-      userDao.save(user) >> user
-
       when: "creating a user"
       def response = userController.createUser(user)
 
       then: "should create a user"
       response.statusCode == HttpStatus.OK
       response.getBody() == user
+
+      cleanup:
+      userController.deleteUserByEmail(user.email)
    }
 
    @Test
@@ -102,9 +88,6 @@ class UserControllerTest extends Specification {
       String lastName = "Jill"
       User user = createUser(email, username, password, firstName, lastName, null)
 
-      and: "mock the database with some users already registered"
-      userDao.findUserByEmailAndUsername(user.getEmail(), user.getUsername()) >> user
-
       when: "creating a user"
       userController.createUser(user)
 
@@ -121,9 +104,6 @@ class UserControllerTest extends Specification {
       String firstName = "Caterine"
       String lastName = "Hoyes"
       User user = createUser(email, username, password, firstName, lastName, null)
-
-      and: "mock the database with some users already registered"
-      userDao.findUserByEmailAndUsername(user.getEmail(), user.getUsername()) >> user
 
       when: "creating a user"
       userController.createUser(user)
@@ -142,12 +122,6 @@ class UserControllerTest extends Specification {
       String lastName = "Bob"
       String alpacaApiKey = "RYFERH6ET5etETGTE6"
       User user = createUser(email, username, password, firstName, lastName, alpacaApiKey)
-
-      and: "mock the database with the same user already registered"
-      userDao.findUserById(user.getId()) >> user
-
-      and: "mock the database with to save the user"
-      userDao.save(user) >> user
 
       when: "registering a key to a user"
       def response = userController.registerAlpacaApiKey(user.getId(), alpacaApiKey)
@@ -191,9 +165,6 @@ class UserControllerTest extends Specification {
       String alpacaApiKey = "RYFERH6ET5etETGTE6"
       User user = createUser(email, username, password, firstName, lastName, alpacaApiKey)
 
-      and: "mock the database with a user that's not found in the database"
-      userDao.findUserByEmailAndUsername(email, username) >> null
-
       when: "registering a key to a user"
       userController.registerAlpacaApiKey(user.getId(), alpacaApiKey)
 
@@ -208,8 +179,6 @@ class UserControllerTest extends Specification {
       User registeredUser = createUser("user.user@money-tree.tech", "usr", password, "user", "user", null)
       //encrypts the password
       userController.createUser(registeredUser)
-      //mock userDao.findUserByEmail()
-      userDao.findUserByEmail(registeredUser.getEmail()) >> registeredUser
 
       and: "A set credentialOk"
       User credentialOk = createCredential(registeredUser.getEmail(), password)
@@ -234,5 +203,8 @@ class UserControllerTest extends Specification {
       userController.login(credentialWrongPassword)
       then: "Should throw CredentialNotFoundException"
       thrown(CredentialNotFoundException)
+
+      cleanup:
+      userController.deleteUserByEmail(registeredUser.getEmail())
    }
 }
