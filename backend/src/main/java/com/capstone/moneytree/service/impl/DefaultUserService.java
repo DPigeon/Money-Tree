8af64daf015470e5e70fc220c813c13fd5baad7a
razine-bensari <bensaria97@gmail.com>
2021-01-17 12:08:54 -0500

    #176-DEFAULT Updating default url to static final var

--- backend/src/main/java/com/capstone/moneytree/service/impl/DefaultUserService.java
+++ backend/src/main/java/com/capstone/moneytree/service/impl/DefaultUserService.java
@@ -29,6 +29,7 @@ import com.capstone.moneytree.validator.ValidatorFactory;
 public class DefaultUserService implements UserService {
 
    private static final String USER_NOT_FOUND = "The requested user was not found";
+   private static final String DEFAULT_PROFILE_NAME = "DEFAULT-profile.jpg";
 
    private final UserDao userDao;
    private final ValidatorFactory validatorFactory;
@@ -37,9 +38,6 @@ public class DefaultUserService implements UserService {
    private final AmazonS3Service amazonS3Service;
 
    private final String bucketName;
-
-   @Value("${aws.default.profile.picture}")
-   private String defaultProfileURL;
 
    @Autowired
    public DefaultUserService(UserDao userDao, ValidatorFactory validatorFactory, AmazonS3Service amazonS3Service, @Value("${aws.profile.pictures.bucket}") String bucketName) {
@@ -81,7 +79,7 @@ public class DefaultUserService implements UserService {
       String password = user.getPassword();
       String encryptedPassword = encryptData(password);
       user.setPassword(encryptedPassword);
-      user.setAvatarURL(String.format("https://%s.s3.amazonaws.com/%s", bucketName, defaultProfileURL));
+      user.setAvatarURL(String.format("https://%s.s3.amazonaws.com/%s", bucketName, DEFAULT_PROFILE_NAME));
 
       userDao.save(user);
 
@@ -96,7 +94,8 @@ public class DefaultUserService implements UserService {
       String imageUrl = amazonS3Service.uploadImageToS3Bucket(imageFile, getBucketName());
 
       //if user already has a profile picture that is not the default picture, handle deleting old picture
-      if (StringUtils.isNotBlank(user.getAvatarURL()) && !user.getAvatarURL().contains(defaultProfileURL)) {
+      if (StringUtils.isNotBlank(user.getAvatarURL()) && !user.getAvatarURL().contains(DEFAULT_PROFILE_NAME)) {
+         String test = "fgh";
          this.amazonS3Service.deleteImageFromS3Bucket(getBucketName(), user.getAvatarURL());
       }
 
