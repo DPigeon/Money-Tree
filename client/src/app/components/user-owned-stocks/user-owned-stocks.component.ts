c4d435f7a67d8fa68a2b155266cd775e035edebe
marwan-ayadi <marwan.a011@gmail.com>
2021-01-20 12:45:27 -0500

    remove unused code

--- client/src/app/components/user-owned-stocks/user-owned-stocks.component.ts
+++ client/src/app/components/user-owned-stocks/user-owned-stocks.component.ts
@@ -35,25 +35,4 @@ export class UserOwnedStocksComponent {
   isUserStockIncrease(): boolean {
     return true;
   }
-
-  displayArrowChange(): string {
-    // arrow_upward or arrow_downward
-    return 'arrow_upward';
-  }
-
-  setPrecisionAbbreviation(stat: number): string {
-    if (stat >= 1000000000000) {
-      // more or equal to 1 trillion
-      return (stat / 1000000000000).toFixed(2) + ' T';
-    } else if (stat >= 1000000000) {
-      // more or equal to 1 billion
-      return (stat / 1000000000).toFixed(2) + ' B';
-    } else if (stat >= 1000000) {
-      return (stat / 1000000).toFixed(2) + ' M'; // more or equal to 1 million
-    } else if (stat >= 10000) {
-      return (stat / 10000).toFixed(2) + ' K'; // more or equal to 10 thousand
-    } else {
-      return stat.toFixed(2);
-    }
-  }
 }
