a78696c71e2d7f80134ef8f1c49122237a69a574
marwan-ayadi <marwan.a011@gmail.com>
2021-01-24 22:35:45 -0500

    Fix test: provide MatDialogRef and MAT_DIALOG_DATA

--- client/src/app/components/sell-owned-stock/sell-owned-actions.component.spec.ts
+++ client/src/app/components/sell-owned-stock/sell-owned-actions.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {
   FORM_MODULE_DPENDENCEIES,
   MATERIAL_MODULE_DEPENDENCIES,
@@ -12,6 +13,15 @@ describe('SellOwnedActionsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MATERIAL_MODULE_DEPENDENCIES, FORM_MODULE_DPENDENCEIES],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {
+            close: (dialogResult: any) => {},
+          },
+        },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+      ],
       declarations: [SellOwnedActionsComponent],
     }).compileComponents();
   });
