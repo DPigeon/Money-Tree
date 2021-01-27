84bcf1575a0752f8a471b0424ddd983ac1448f8d
razine-bensari <bensaria97@gmail.com>
2021-01-26 21:53:31 -0500

    #229-BUY Fixing bug

--- backend/src/main/java/com/capstone/moneytree/facade/AmazonS3Facade.java
+++ backend/src/main/java/com/capstone/moneytree/facade/AmazonS3Facade.java
@@ -7,7 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
 
-import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
