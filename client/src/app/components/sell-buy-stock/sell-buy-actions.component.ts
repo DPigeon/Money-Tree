cd515a382236db04785054e0b8651d648dc979a6
Alessandro <kalessandro14@gmail.com>
2021-02-08 15:55:36 -0500

    buy/sell popup logic #232

--- client/src/app/components/sell-buy-stock/sell-buy-actions.component.ts
+++ client/src/app/components/sell-buy-stock/sell-buy-actions.component.ts
@@ -1,15 +1,16 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { StoreFacadeService } from 'src/app/store/store-facade.service';
 import { Stock } from '../../interfaces/stock';
 import { Transaction } from '../../interfaces/transaction';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-sell-buy-actions',
   templateUrl: './sell-buy-actions.component.html',
   styleUrls: ['./sell-buy-actions.component.scss'],
 })
-export class SellOrBuyActionsComponent implements OnInit {
+export class SellOrBuyActionsComponent {
   isMarketOrder = true;
   quantity = 0;
   price = 0;
@@ -17,19 +18,29 @@ export class SellOrBuyActionsComponent implements OnInit {
   currentStock: Stock;
   balance: number = 5000; // Faking this value for now, will have to get this from alpaca
   remainingBalance: number = 0
+  
   constructor(
     public dialogRef: MatDialogRef<SellOrBuyActionsComponent>,
     private storeFacade: StoreFacadeService,
     @Inject(MAT_DIALOG_DATA) public data: {
       type: string,
-      stock: Stock
+      stockInfo: Stock,
+      userInfo: User
     }
   ) {
-    this.currentStock = data.stock;
-    this.price = this.currentStock.stockValue;
+    this.currentStock = data.stockInfo;
+    this.price = this.currentStock ? this.currentStock.stockValue : 0;
     this.remainingBalance = this.balance;
   }
-  ngOnInit(): void { }
+
+  get stockPrice(): number {
+    return this.data && this.data.stockInfo ? this.data.stockInfo.stockValue : 0;
+  }
+
+  get stockName(): string {
+    return this.data && this.data.stockInfo ? this.data.stockInfo.companyName : '';
+  }
+
   clearInputs(): void {
     this.quantity = 0;
     this.price = 0;
@@ -41,18 +52,18 @@ export class SellOrBuyActionsComponent implements OnInit {
 
   processActions(): void {
     const transaction: Transaction = {
-      symbol: this.data.stock.tickerSymbol,
+      symbol: this.data.stockInfo.tickerSymbol,
       qty: this.quantity,
       side: this.data.type,
       type: this.isMarketOrder ? "market" : "limit",
       time_in_force: "day"
     }
-    this.storeFacade.processStockTransaction(transaction);
+    this.storeFacade.processStockTransaction(transaction, this.data.userInfo.id);
     this.dialogRef.close();
   }
   
   getTotal(): number {
-    return this.isMarketOrder ? this.quantity * this.currentStock.stockValue : this.quantity * this.price;
+    return this.isMarketOrder && this.currentStock ? this.quantity * this.currentStock.stockValue : this.quantity * this.price;
   }
 
   getRemainingBalance(): number {
@@ -61,6 +72,6 @@ export class SellOrBuyActionsComponent implements OnInit {
   }
 
   getStockPrice(): number {
-    return (this.data != null && this.data.stock.stockValue != null) ? this.data.stock.stockValue : 0;
+    return (this.data && this.data.stockInfo && this.data.stockInfo.stockValue) ? this.data.stockInfo.stockValue : 0;
   }
 }
