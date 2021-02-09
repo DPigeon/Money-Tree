04937959eb6bc6fab20e6048f78e9dfa16d56bc0
Alessandro <kalessandro14@gmail.com>
2021-02-09 14:43:12 -0500

    format user in transaction #232

--- client/src/app/services/transaction/transaction.service.ts
+++ client/src/app/services/transaction/transaction.service.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../api/api.service';
 import { Transaction } from '../../interfaces/transaction';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { User } from 'src/app/interfaces/user';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +14,29 @@ export class TransactionService {
 
   processStockTransaction(transaction: Transaction, userId: number): Observable<any> {
     return this.api.post('transactions/execute/' + userId, transaction)
-    .pipe(map((res: Response) => console.log(res)));
+    .pipe(map((res: Response) => this.userFormatter(res.body)));
+  }
+
+  userFormatter(response: any): User {
+    const formattedUser: User = {
+      id: response.id,
+      firstName: response.firstName,
+      lastName: response.lastName,
+      username: response.username,
+      avatarURL: response.avatarURL,
+      coverPhotoURL: response.coverPhotoURL,
+      email: response.email,
+      score: response.score,
+      rank: response.rank,
+      balance: response.balance,
+      alpacaApiKey: response.alpacaApiKey,
+      follows: response.follows,
+      followers: response.followers,
+      portfolio: response.portfolio,
+      transactions: response.transactions,
+      biography: response.biography,
+    };
+    return formattedUser;
   }
 
 }
