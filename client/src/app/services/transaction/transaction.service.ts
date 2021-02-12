ce5496041e64b43e14c8aa1a5e6385b9414a80cc
Alessandro <kalessandro14@gmail.com>
2021-02-12 17:30:51 -0500

    removed duplicate code #232

--- client/src/app/services/transaction/transaction.service.ts
+++ client/src/app/services/transaction/transaction.service.ts
@@ -3,42 +3,19 @@ import { ApiService } from '../api/api.service';
 import { Transaction } from '../../interfaces/transaction';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { User } from 'src/app/interfaces/user';
+import { UserService } from '../user/user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class TransactionService {
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService, private userService: UserService) {}
 
   processStockTransaction(transaction: Transaction, userId: number): Observable<any> {
     return this.api.post('transactions/execute/' + userId, transaction)
-    .pipe(map((res: Response) => this.userFormatter(res.body)));
+    .pipe(map((res: Response) => this.userService.userFormatter(res.body)));
   }
-
-  userFormatter(response: any): User {
-    const formattedUser: User = {
-      id: response.id,
-      firstName: response.firstName,
-      lastName: response.lastName,
-      username: response.username,
-      avatarURL: response.avatarURL,
-      coverPhotoURL: response.coverPhotoURL,
-      email: response.email,
-      score: response.score,
-      rank: response.rank,
-      balance: response.balance,
-      alpacaApiKey: response.alpacaApiKey,
-      follows: response.follows,
-      followers: response.followers,
-      portfolio: response.portfolio,
-      transactions: response.transactions,
-      biography: response.biography,
-    };
-    return formattedUser;
-  }
-
 }
 
 
