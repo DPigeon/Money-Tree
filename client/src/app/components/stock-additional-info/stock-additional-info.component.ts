3cdf8550c023d75140bbe94674b50ec5ddd8cae0
marwan-ayadi <marwan.a011@gmail.com>
2021-02-16 21:18:55 -0500

    Update basic test

--- client/src/app/components/stock-additional-info/stock-additional-info.component.ts
+++ client/src/app/components/stock-additional-info/stock-additional-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Stock } from '../../interfaces/stock';
 export interface Follower {
