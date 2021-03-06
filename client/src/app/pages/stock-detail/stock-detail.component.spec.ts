15f56501fd688ca7da4c5784e27bf4244a44c371
EspressoCode <hn.84@yahoo.com>
2021-03-06 00:59:50 -0800

    #267 fixed the failing test for stock-detail

--- client/src/app/pages/stock-detail/stock-detail.component.spec.ts
+++ client/src/app/pages/stock-detail/stock-detail.component.spec.ts
@@ -1,65 +1,132 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { StockDetailComponent } from './stock-detail.component';
-import { StockDetailHeaderComponent } from '../../components/stock-detail-header/stock-detail-header.component';
-import { StockStatsComponent } from '../../components/stock-stats/stock-stats.component';
-import { StoreFacadeService } from '../../store/store-facade.service';
+// tslint:disable
+import { TestBed } from '@angular/core/testing';
 import {
-  FORM_MODULE_DPENDENCEIES,
-  MATERIAL_MODULE_DEPENDENCIES,
-  NGRX_STORE_MODULE,
-} from '../../shared.module';
-import { ActivatedRoute } from '@angular/router';
-import { HeaderComponent } from 'src/app/components/header/header.component';
-import { UserOwnedStocksComponent } from 'src/app/components/user-owned-stocks/user-owned-stocks.component';
-import { StockSearchComponent } from 'src/app/components/stock-search/stock-search.component';
-import { StockAdditionalInfoComponent } from 'src/app/components/stock-additional-info/stock-additional-info.component';
-import { RouterTestingModule } from '@angular/router/testing';
+  Pipe,
+  PipeTransform,
+  Injectable,
+  CUSTOM_ELEMENTS_SCHEMA,
+  NO_ERRORS_SCHEMA,
+  Directive,
+  Input,
+} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { of as observableOf } from 'rxjs';
 
-const fakeActivatedRoute = {
-  snapshot: {
-    paramMap: {
-      get(): string {
-        return 'AC';
-      },
-    },
-  },
-} as any;
+import { StockDetailComponent } from './stock-detail.component';
+import { StoreFacadeService } from '../../store/store-facade.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Injectable()
+class MockStoreFacadeService {
+  stockHistoricalDataLoaded$ = {};
+  currentStockLoaded$ = {};
+  currentMarketClock$ = {};
+  currentUser$ = {};
+}
+
+@Injectable()
+class MockRouter {
+  navigate() {}
+}
+
+@Directive({ selector: '[oneviewPermitted]' })
+class OneviewPermittedDirective {
+  @Input() oneviewPermitted;
+}
+
+@Pipe({ name: 'translate' })
+class TranslatePipe implements PipeTransform {
+  transform(value) {
+    return value;
+  }
+}
+
+@Pipe({ name: 'phoneNumber' })
+class PhoneNumberPipe implements PipeTransform {
+  transform(value) {
+    return value;
+  }
+}
+
+@Pipe({ name: 'safeHtml' })
+class SafeHtmlPipe implements PipeTransform {
+  transform(value) {
+    return value;
+  }
+}
 
 describe('StockDetailComponent', () => {
-  let component: StockDetailComponent;
-  let fixture: ComponentFixture<StockDetailComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        MATERIAL_MODULE_DEPENDENCIES,
-        RouterTestingModule,
-        FORM_MODULE_DPENDENCEIES,
-      ],
-      declarations: [
-        StockDetailComponent,
-        StockDetailHeaderComponent,
-        StockStatsComponent,
-        UserOwnedStocksComponent,
-        StockAdditionalInfoComponent,
-        HeaderComponent,
-        StockSearchComponent,
-      ],
-      providers: [
-        StoreFacadeService,
-        { provide: ActivatedRoute, useValue: fakeActivatedRoute },
-        NGRX_STORE_MODULE,
-      ],
-    }).compileComponents();
-  });
+  let fixture;
+  let component;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule],
+      declarations: [
+        StockDetailComponent,
+        TranslatePipe,
+        PhoneNumberPipe,
+        SafeHtmlPipe,
+        OneviewPermittedDirective,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: StoreFacadeService, useClass: MockStoreFacadeService },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { url: 'url', params: {}, queryParams: {}, data: {} },
+            url: observableOf('url'),
+            params: observableOf({}),
+            queryParams: observableOf({}),
+            fragment: observableOf('fragment'),
+            data: observableOf({}),
+          },
+        },
+        { provide: Router, useClass: MockRouter },
+      ],
+    })
+      .overrideComponent(StockDetailComponent, {})
+      .compileComponents();
     fixture = TestBed.createComponent(StockDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.debugElement.componentInstance;
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    component.ngOnDestroy = function () {};
+    fixture.destroy();
+  });
+
+  it('should run #constructor()', async () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should run #ngOnInit()', async () => {
+    component.stockHistoricalData$ = component.stockHistoricalData$ || {};
+    component.stockHistoricalData$.subscribe = jest
+      .fn()
+      .mockReturnValue([null]);
+    component.route = component.route || {};
+    component.route.snapshot = {
+      paramMap: {
+        get: function () {},
+      },
+    };
+    component.storeFacade = component.storeFacade || {};
+    component.storeFacade.loadCurrentStock = jest.fn();
+    component.storeFacade.loadCurrentStockHistoricalData = jest.fn();
+    component.storeFacade.loadMarketClock = jest.fn();
+    component.router = component.router || {};
+    component.router.events = observableOf({});
+    component.ngOnInit();
+  });
+
+  it('should run #changeChartRangeInterval()', async () => {
+    component.storeFacade = component.storeFacade || {};
+    component.storeFacade.loadCurrentStockHistoricalData = jest.fn();
+    component.changeChartRangeInterval({
+      range: {},
+      interval: {},
+    });
   });
 });
