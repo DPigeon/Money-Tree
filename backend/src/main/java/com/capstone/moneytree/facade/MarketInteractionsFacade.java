0c10ca75770adc69981083b0d85d9ca78092495a
DPigeon <D_Pig@encs.concordia.ca>
2021-03-08 01:12:54 -0500

    #319: Fix code smell

--- backend/src/main/java/com/capstone/moneytree/facade/MarketInteractionsFacade.java
+++ backend/src/main/java/com/capstone/moneytree/facade/MarketInteractionsFacade.java
@@ -19,7 +19,6 @@ import javax.validation.constraints.NotNull;
 import com.capstone.moneytree.dao.UserDao;
 import com.capstone.moneytree.dao.MadeDao;
 import com.capstone.moneytree.dao.OwnsDao;
-import com.capstone.moneytree.dao.ToFulfillDao;
 import com.capstone.moneytree.model.TransactionStatus;
 import com.capstone.moneytree.model.node.Stock;
 import com.capstone.moneytree.model.node.Transaction;
