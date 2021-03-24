3d9254df50dc4e3cd90caf7359e49c91da430086
EspressoCode <hn.84@yahoo.com>
2021-03-24 01:50:02 -0700

    #343 fixed the code smell

--- client/src/app/store/effects/app.effects.ts
+++ client/src/app/store/effects/app.effects.ts
@@ -1,4 +1,3 @@
-import { setCurrentLeaderboardUsers } from './../actions/app.actions';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
