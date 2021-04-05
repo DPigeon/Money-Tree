b51f0b561e2c536cc70e896d65cbb6ca34793777
EspressoCode <hn.84@yahoo.com>
2021-04-05 13:36:33 -0700

    fixed the portfolio amounts on stock detail page

--- client/src/app/components/user-owned-stocks/user-owned-stocks.component.ts
+++ client/src/app/components/user-owned-stocks/user-owned-stocks.component.ts
@@ -1,35 +1,45 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { AlpacaUserPosition } from 'src/app/interfaces/alpacaPosition';
 import { Stock } from 'src/app/interfaces/stock';
 import { User } from 'src/app/interfaces/user';
 import { SellOrBuyActionsComponent } from '../sell-buy-stock/sell-buy-actions.component';
 
 export interface OwnedStocksStats {
-  noOfShares?: number;
-  totalOwned?: number; // total value of all the stocks this user owns (to calculate portfolio %)
-  thisStockTotal?: number; // total value if this stock owned by the user
-  averagePerShare?: number;
-  percentPortfolio?: number;
-  profitOrLossAmount?: number;
-  profitOrLossPercent?: number;
+  noOfShares?: string;
+  averagePerShare?: string;
+  percentPortfolio?: string;
+  thisStockTotal?: number;
+  profitOrLossAmount?: string;
+  profitOrLossPercent?: string;
 }
 @Component({
   selector: 'app-user-stocks',
   templateUrl: './user-owned-stocks.component.html',
   styleUrls: ['./user-owned-stocks.component.scss'],
 })
-export class UserOwnedStocksComponent implements OnChanges {
+export class UserOwnedStocksComponent implements OnInit, OnChanges {
   @Input() stockInfo: Stock;
-  @Input() userOwnedStocks: Stock[];
   @Input() userInfo: User;
+  @Input() alpacaPositions$: Observable<AlpacaUserPosition[]>;
+  alpacaPositions: AlpacaUserPosition[];
 
   ownedStocksStats: OwnedStocksStats;
 
   constructor(public dialog: MatDialog) {}
   ngOnChanges(): void {
-    if (this.userOwnedStocks) {
-      this.calculateOwnedStockInfo();
-    }
+    this.calculateOwnedStockInfo();
+    console.log('called');
+  }
+  ngOnInit(): void {
+    this.alpacaPositions$.subscribe((data) => (this.alpacaPositions = data));
   }
 
   get stockSymbol(): string {
@@ -41,44 +51,48 @@ export class UserOwnedStocksComponent implements OnChanges {
   }
 
   get userStockNoOfShares(): number {
-    return this.ownedStocksStats ? this.ownedStocksStats.noOfShares : 0;
+    return this.ownedStocksStats ? Number(this.ownedStocksStats.noOfShares) : 0;
   }
 
   get userStockAvgShare(): string {
     return this.ownedStocksStats
-      ? this.ownedStocksStats.averagePerShare.toFixed(2)
+      ? Number(this.ownedStocksStats.averagePerShare).toFixed(2)
       : '0';
   }
 
   get userStockPortfolio(): string {
     return this.ownedStocksStats
-      ? this.ownedStocksStats.percentPortfolio.toFixed(2)
+      ? Number(this.ownedStocksStats.percentPortfolio).toFixed(2)
       : '0';
   }
 
   get userStockTotalValue(): string {
     return this.ownedStocksStats
-      ? this.ownedStocksStats.thisStockTotal.toFixed(2)
+      ? Number(this.ownedStocksStats.thisStockTotal).toFixed(2)
       : '0';
   }
 
   get profitOrLossAmount(): string {
     if (this.ownedStocksStats) {
-      return Math.abs(this.ownedStocksStats.profitOrLossAmount).toFixed(2);
+      return Math.abs(Number(this.ownedStocksStats.profitOrLossAmount)).toFixed(
+        2
+      );
     }
     return '0';
   }
 
   get profitOrLossPercent(): string {
     if (this.ownedStocksStats) {
-      return Math.abs(this.ownedStocksStats.profitOrLossPercent).toFixed(2);
+      return Math.abs(
+        Number(this.ownedStocksStats.profitOrLossPercent)
+      ).toFixed(2);
     }
     return '0';
   }
 
   isUserGained(): boolean {
     if (this.ownedStocksStats) {
-      return this.ownedStocksStats.profitOrLossAmount >= 0;
+      return Number(this.ownedStocksStats.profitOrLossAmount) >= 0;
     }
   }
   stockChangeColor(): string {
@@ -88,49 +102,42 @@ export class UserOwnedStocksComponent implements OnChanges {
   }
 
   userHasThisStock(): boolean {
-    return this.ownedStocksStats ? this.ownedStocksStats.noOfShares > 0 : false;
+    return this.ownedStocksStats
+      ? this.ownedStocksStats.noOfShares !== '0'
+      : false;
   }
 
   calculateOwnedStockInfo(): void {
-    if (this.userOwnedStocks && this.stockInfo) {
+    if (this.alpacaPositions) {
       this.ownedStocksStats = {
-        noOfShares: 0,
-        totalOwned: 0,
+        noOfShares: '0',
+        averagePerShare: '0',
+        percentPortfolio: '0',
         thisStockTotal: 0,
-        averagePerShare: 0,
-        percentPortfolio: 0,
-        profitOrLossAmount: 0,
-        profitOrLossPercent: 0,
+        profitOrLossAmount: '0',
+        profitOrLossPercent: '0',
       };
-      for (const stock of this.userOwnedStocks) {
-        this.ownedStocksStats.totalOwned +=
-          Number(stock.avgPrice) * Number(stock.quantity);
-
-        if (stock.tickerSymbol === this.stockInfo.tickerSymbol) {
-          this.ownedStocksStats.noOfShares += Number(stock.quantity);
-          this.ownedStocksStats.thisStockTotal +=
-            Number(stock.avgPrice) * Number(stock.quantity);
+      let totalStocksValue = 0;
+      for (const position of this.alpacaPositions) {
+        totalStocksValue += Number(position.currentValue);
+        if (position.symbol === this.stockInfo.tickerSymbol) {
+          this.ownedStocksStats.thisStockTotal += Number(position.currentValue);
+          this.ownedStocksStats.noOfShares = position.qty;
+          this.ownedStocksStats.averagePerShare = Number(
+            position.avgPrice
+          ).toFixed(2);
+          this.ownedStocksStats.profitOrLossPercent = (
+            Number(position.gainPercentage) * 100
+          ).toFixed(2);
+          this.ownedStocksStats.profitOrLossAmount = Number(
+            position.gainAmount
+          ).toFixed(2);
         }
       }
-      this.ownedStocksStats.averagePerShare =
-        this.ownedStocksStats.thisStockTotal / this.ownedStocksStats.noOfShares;
-
-      this.ownedStocksStats.percentPortfolio =
-        (this.ownedStocksStats.thisStockTotal /
-          this.ownedStocksStats.totalOwned) *
-        100;
-
-      const realTimePrice =
-        this.ownedStocksStats.noOfShares * this.stockInfo.stockValue;
-
-      this.ownedStocksStats.profitOrLossAmount =
-        this.ownedStocksStats.thisStockTotal - // (total spent on this stock - real-time price)
-        realTimePrice;
-
-      this.ownedStocksStats.profitOrLossPercent =
-        ((this.ownedStocksStats.averagePerShare - this.stockInfo.stockValue) / //  [(avg - real avg)/real avg]  * 100
-          this.stockInfo.stockValue) *
-        100;
+      this.ownedStocksStats.percentPortfolio = (
+        (this.ownedStocksStats.thisStockTotal / totalStocksValue) *
+        100
+      ).toFixed(2);
     }
   }
 
