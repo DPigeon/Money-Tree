1681e080f2c003ba48340384193ea8edab5c2f4c
EspressoCode <hn.84@yahoo.com>
2021-04-07 22:59:14 -0700

    added more tests to frontend excluded data-formatters.ts from sonarcloud coverage

--- client/src/app/components/user-owned-stocks/user-owned-stocks.component.spec.ts
+++ client/src/app/components/user-owned-stocks/user-owned-stocks.component.spec.ts
@@ -1,4 +1,3 @@
-import { AlpacaUserPosition } from 'src/app/interfaces/alpacaPosition';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Stock } from 'src/app/interfaces/stock';
 import { StoreFacadeService } from 'src/app/store/store-facade.service';
@@ -8,7 +7,10 @@ import {
   NGRX_STORE_MODULE,
   NGX_ECHART,
 } from '../../shared.module';
-import { UserOwnedStocksComponent } from './user-owned-stocks.component';
+import {
+  UserOwnedStocksComponent,
+  OwnedStocksStats,
+} from './user-owned-stocks.component';
 import { StockDetailHeaderComponent } from '../stock-detail-header/stock-detail-header.component';
 import { StockStatsComponent } from '../stock-stats/stock-stats.component';
 import { StockAdditionalInfoComponent } from '../stock-additional-info/stock-additional-info.component';
@@ -18,6 +20,7 @@ import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { StockDetailComponent } from './../../pages/stock-detail/stock-detail.component';
 import { HistoricalChartComponent } from '../historical-chart/historical-chart.component';
+import { AlpacaUserPosition } from './../../interfaces/alpacaPosition';
 import { Observable } from 'rxjs';
 
 // integration tests
@@ -28,48 +31,35 @@ describe('UserOwnedStocksComponent', () => {
   const stockInfo: Stock = {
     companyName: 'Apple inc',
     tickerSymbol: 'AAPL',
-    stockValue: 110, // real-time average price
+    stockValue: 125, // real-time average price
   };
-  const userOwnedStocks: Stock[] = [
-    {
-      companyName: 'Apple inc',
-      tickerSymbol: 'AAPL',
-      quantity: '10',
-      avgPrice: '100',
-    },
-    {
-      companyName: 'Apple inc',
-      tickerSymbol: 'AAPL',
-      quantity: '20',
-      avgPrice: '80',
-    },
-    {
-      companyName: 'Apple inc',
-      tickerSymbol: 'AAPL',
-      quantity: '7',
-      avgPrice: '30',
-    },
-    {
-      companyName: 'Tesla',
-      tickerSymbol: 'TSLA',
-      quantity: '5',
-      avgPrice: '1000',
-    },
-    /*
-    / with the above stocks we will have 37 shares of apple and 5 shares of tesla
-    / total spent on all of the stocks would be : $7810
-    / total spent on Apple would be $2810 with the average price of: 2810/37 = 75.95
-    / percent portfolio for apple stocks would be 2810/7810 * 100 = 35.98
-    / profit/loss amount would be: (75.95 * 37)-(110*37) = -1260.00 (loss), absolute value
-    / profit/loss percent would be: [(75.94-110)/110] * 100 = -30.96 %, absolute value
-    */
-  ];
+  const ownedStocksStats: OwnedStocksStats = {
+    noOfShares: '0',
+    averagePerShare: '0',
+    percentPortfolio: '0',
+    thisStockTotal: 0,
+    profitOrLossAmount: '0',
+    profitOrLossPercent: '0',
+  };
 
+  const alpacaPositions: AlpacaUserPosition[] = [
+    {
+      symbol: 'AAPL',
+      avgPrice: '120',
+      qty: '5',
+      cost: '600',
+      currentPrice: '125',
+      currentValue: '600',
+      gainAmount: '25',
+      change: '1',
+      gainPercentage: '0.41',
+    },
+  ];
   const fakeActivatedRoute = {
     snapshot: {
       paramMap: {
         get(): string {
-          return 'AC';
+          return 'AAPL';
         },
       },
     },
@@ -105,61 +95,39 @@ describe('UserOwnedStocksComponent', () => {
     fixture = TestBed.createComponent(UserOwnedStocksComponent);
     component = fixture.componentInstance;
     component.stockInfo = stockInfo;
-    component.alpacaPositions$ = new Observable<AlpacaUserPosition[]>();
+    component.alpacaPositions$ = new Observable<any>();
+    component.alpacaPositions = alpacaPositions;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('it should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should calculate stock stats correctly', () => {
-  //   component.calculateOwnedStockInfo();
-  //   expect(component.stockSymbol).toBe('AAPL');
-  //   expect(component.stockCompanyName).toBe('Apple inc');
-  //   expect(component.userStockNoOfShares).toBe(37);
-  //   expect(component.userStockAvgShare).toBe('75.95');
-  //   expect(component.userStockPortfolio).toBe('35.98');
-  //   expect(component.profitOrLossAmount).toBe('1260.00'); // absolute value
-  //   expect(component.profitOrLossPercent).toBe('30.96'); // absolute value
-  //   expect(component.userStockTotalValue).toBe('2810.00');
-  // });
+  it('it should calculate stock stats correctly', () => {
+    component.calculateOwnedStockInfo();
+    expect(component.stockSymbol).toBe('AAPL');
+    expect(component.stockCompanyName).toBe('Apple inc');
+    expect(component.userStockNoOfShares).toBe(5);
+    expect(component.userStockAvgShare).toBe('120.00');
+    expect(component.userStockPortfolio).toBe('100.00');
+    expect(component.profitOrLossAmount).toBe('25.00');
+    expect(component.profitOrLossPercent).toBe('41.00');
+    expect(component.userStockTotalValue).toBe('600.00');
+  });
 
-  // it('should return "" if stockInfo has not beeen yet loaded', () => {
-  //   component.stockInfo = null;
-  //   expect(component.stockSymbol).toBe('');
-  //   expect(component.stockCompanyName).toBe('');
-  // });
+  it('it should show user stock info only if user has this stock ', () => {
+    component.calculateOwnedStockInfo();
+    expect(component.userHasThisStock()).toBe(true);
+  });
 
-  // it('should return 0 if userOwnedStockStats has not beeen yet calculated', () => {
-  //   component.calculateOwnedStockInfo();
-  //   expect(component.userStockNoOfShares).toBe(0);
-  //   expect(component.userStockAvgShare).toBe('0');
-  //   expect(component.userStockPortfolio).toBe('0');
-  //   expect(component.profitOrLossAmount).toBe('0'); // absolute value
-  //   expect(component.profitOrLossPercent).toBe('0'); // absolute value
-  //   expect(component.userStockTotalValue).toBe('0');
-  // });
+  it('it should show the right color for +/- gain amounts ', () => {
+    component.calculateOwnedStockInfo();
+    expect(component.isUserGained()).toBe(true);
+    expect(component.stockChangeColor()).toEqual('positive-change');
 
-  // it('should see if the user is in profit or loss', () => {
-  //   component.calculateOwnedStockInfo();
-  //   expect(component.isUserGained()).toBe(false);
-  //   expect(component.stockChangeColor()).toBe('negative-change');
-  //   // reducing the value of stock so that user goes into profit
-  //   component.stockInfo.stockValue = 50;
-  //   component.calculateOwnedStockInfo();
-  //   expect(component.isUserGained()).toBe(true);
-  //   expect(component.stockChangeColor()).toBe('positive-change');
-  // });
-
-  // it('should not show the stats if user does not have the stock', () => {
-  //   component.calculateOwnedStockInfo();
-  //   expect(component.ownedStocksStats.noOfShares).toBe('37');
-  //   expect(component.userHasThisStock()).toBe(true);
-  //   // changing to a stock that user does not have:
-  //   component.stockInfo.tickerSymbol = 'SOMEOTHERSTOCK';
-  //   component.calculateOwnedStockInfo();
-  //   expect(component.ownedStocksStats.noOfShares).toBe('0');
-  //   expect(component.userHasThisStock()).toBe(false);
-  // });
+    component.ownedStocksStats.profitOrLossAmount = '-5.00';
+    expect(component.isUserGained()).toBe(false);
+    expect(component.stockChangeColor()).toEqual('negative-change');
+  });
 });
